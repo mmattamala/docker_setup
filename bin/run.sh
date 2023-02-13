cba9ab19f30f34b268d5222b03a92c1e487bd8eb
Matias Mattamala <matias@robots.ox.ac.uk>
2023-02-13 16:27:05 +0000

    Fix touch issue

--- bin/run.sh
+++ bin/run.sh
@@ -141,7 +141,7 @@ fi
 XSOCK=/tmp/.X11-unix
 XAUTH=/tmp/.docker.xauth
 if [ ! -f $XAUTH ]; then
-    touch $XAUTH
+    > $XAUTH # make an empty file
     xauth nlist $DISPLAY | sed -e 's/^..../ffff/' | xauth -f $XAUTH nmerge -
     chmod a+r $XAUTH
 fi
