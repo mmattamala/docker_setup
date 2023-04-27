9cdc29ae71c3ade963c515b28648670697b7445b
Matias Mattamala <matias@robots.ox.ac.uk>
2023-04-27 21:51:20 +0100

    Minor fix in orin entrypoint

--- entrypoints/jetson_orin_coyote.sh
+++ entrypoints/jetson_orin_coyote.sh
@@ -4,9 +4,6 @@
 # Fix for skimage
 export LD_PRELOAD=/usr/local/lib/python3.8/dist-packages/skimage/_shared/../../scikit_image.libs/libgomp-d22c30c5.so.1.0.0
 
-# Setup ROS_IP to LPC
-echo "ros_network 192.168.0.41 > /dev/null" >> ~/.bashrc
-
 # Install wvn
 install_wvn="pip3 install -e /root/git/wild_visual_navigation"
 
